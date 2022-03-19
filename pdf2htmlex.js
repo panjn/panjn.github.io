9c8ce2be9d9cf827b191608a6aea8943738edaa3
panjianning <panjianning@huya.com>
2022-03-19 23:55:01 +0800

    smaller

--- pdf2htmlex.js
+++ pdf2htmlex.js
@@ -1,5 +1,4 @@
 var pdf = document.getElementById('page-container');
 var contents = document.getElementsByClassName('content');
 var content = contents[0];
-var w0Width = document.getElementsByClassName('w0')[0].style.width;
-pdf.style.zoom = content.getBoundingClientRect().width / parseInt(w0Width.substring(0, w0Width.length-2));
+pdf.style.zoom = content.getBoundingClientRect().width / 720;
