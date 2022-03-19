9c98606dcd4d396f8f84ae09125b171351054139
panjianning <panjianning@huya.com>
2022-03-19 22:30:51 +0800

    pinterest

--- pdf2htmlex.js
+++ pdf2htmlex.js
@@ -1,4 +1,5 @@
 var pdf = document.getElementById('page-container');
 var contents = document.getElementsByClassName('content');
 var content = contents[0];
-pdf.style.zoom = content.getBoundingClientRect().width / 720;
+var w0Width = document.getElementsByClassName('w0')[0].style.width;
+pdf.style.zoom = content.getBoundingClientRect().width / parseInt(w0Width.substring(0, w0Width.length-2));
