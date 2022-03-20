51a79bab7c0fa4a5a10c0fd07f1a9f92de34f94f
panjianning <panjianning@huya.com>
2022-03-20 17:48:17 +0800

    service mesh

--- js/pdf2htmlex.js
+++ js/pdf2htmlex.js
@@ -1,10 +1,31 @@
+var contents = document.getElementsByClassName('content');
+var content = contents[0];
+
 function fitContent() {
     var pdf = document.getElementById('page-container');
-    var contents = document.getElementsByClassName('content');
-    var content = contents[0];
     pdf.style.zoom = content.getBoundingClientRect().width / 720;
-};
+}
 fitContent();
 window.addEventListener('resize', fitContent);
+
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+    var pageElem = document.getElementsByClassName("w0")[0];
+    var pageHeight = (pageElem.offsetHeight+13) * content.getBoundingClientRect().width / 720;
+
+    if (e.keyCode === 37) {
+        e.preventDefault();
+        window.scrollBy(0, -pageHeight);
+    }
+    else if (e.keyCode === 39) {
+        e.preventDefault();
+        window.scrollBy(0, pageHeight);
+    }
+}
+
 
 
